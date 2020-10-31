f8dde2cda775790dc7d9adad122561816b63850b
TSGames <anfragen@tsgames.de>
2020-10-31 19:05:13 +0100

    Improved, shutdown

--- light.py
+++ light.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 # NeoPixel library strandtest example
 # Author: Tony DiCola (tony@tonydicola.com)
 #
@@ -9,18 +9,19 @@ import time
 from neopixel import *
 import argparse
 import psutil
- 
+import signal
+
 COLOR = [0,30,255]
 # LED strip configuration:
 LED_COUNT = 12 # Number of LED pixels.
-LED_PIN = 18 # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN = 12 # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN = 10 # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10 # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0 # set to '1' for GPIOs 13, 19, 41, 45 or 53
- 
+
 def mix(color, strength):
     return Color(int(color[0] * strength), int(color[1] * strength), int(color[2] * strength))
 
@@ -28,11 +29,20 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
 strip.begin()
 pos = 0.0
 load = 0
+
+def shutdown(_signo, _stack_frame):
+    for i in range(LED_COUNT):
+        strip.setPixelColor(i, Color(0, 0, 0))
+    strip.show()
+    exit(0)
+
+signal.signal(signal.SIGTERM, shutdown)
+
 while True:
     rawLoad = psutil.cpu_percent(interval=0.01) / 100
     load = load + (rawLoad - load) * 0.01
     #print(load)
-    loadFac = 0.2 + load*0.8
+    loadFac = 0.1 + load*0.9
     for i in range(LED_COUNT):
         strength = abs(i - pos) / LED_COUNT
         if strength < 0.5:
